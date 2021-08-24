7072a8e0a165f90cfc6b0c0118db89d7771bf2eb
Fukuda55 <fukuda@defide.co.jp>
2021-08-24 09:40:21 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+  ]
+export default router
