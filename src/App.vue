8ae9e6ef70b67a3e38a0488917d8a91f2e98feaf
Fukuda55 <fukuda@defide.co.jp>
2021-08-26 19:09:26 +0900

    環境構築

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <Navigation />
+    <Header />
+    <Sidepanel />
+    <transition name="fade">
+      <router-view />
+    </transition>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Footer from "./views/Footer.vue";
+import Header from "./views/Header.vue";
+import Navigation from "./views/Navigation.vue";
+import Sidepanel from "./views/Sidepanel.vue";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Footer,
+    Header,
+    Navigation,
+    Sidepanel,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
