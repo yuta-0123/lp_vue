fbd8e791a963dd3729b973644494a747f8ceaf70
Fukuda55 <fukuda@defide.co.jp>
2021-08-31 15:19:13 +0900

    scss 修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,7 +39,7 @@
             <p>住宅購入に関するセミナーをみてみよう</p>
           </div>
           <p class="main_subMessage">
-            住宅購入に関して、事前に知っておきたいことや、<br>気になることはありませんか？<br>まずは知識を身に付けるために<br>セミナー講座を見てみましょう。
+            住宅購入に関して、事前に知っておきたいことや、<br />気になることはありませんか？<br />まずは知識を身に付けるために<br />セミナー講座を見てみましょう。
           </p>
         </div>
         <div class="main_seminar main_bg">
@@ -47,28 +47,43 @@
             <p>チャットで気軽に質問</p>
           </div>
           <p class="main_subMessage">
-            質問したいと思っても電話だとためらってしまう・・<br>そんなときにチャットなら気軽に質問できます。
+            質問したいと思っても電話だとためらってしまう・・<br />そんなときにチャットなら気軽に質問できます。
           </p>
         </div>
         <div class="main_video main_bg">
-            <div class="main_video_message">
-              <p>具体的なお金の話を直接聞いてみたい</p>
+          <div class="main_video_message">
+            <p>具体的なお金の話を直接聞いてみたい</p>
+          </div>
+          <p class="main_subMessage">
+            具体的な提案を受けたい、質問したいことがたくさんある、そんな時は1on1のビデオ会議を活用しましょう。
+          </p>
+        </div>
+        <div class="main_simulation main_bg">
+          <div class="main_simulation_message">
+            <p></p>
+          </div>
+          <p class="main_subMessage">
+            ビデオ会議しながらその場でプラン出力や、<br />各カテゴリでの絞り込みが可能です。
+          </p>
+        </div>
+        <div class="main_story">
+          <div class="main_story_01 rellax" data-rellax-speed="-5">
+            <div class="main_story_01_img">
+              <img src="@/assets/img/conference_1280.jpg" alt="" />
             </div>
-            <p class="main_subMessage">
-              具体的な提案を受けたい、質問したいことがたくさんある、そんな時は1on1のビデオ会議を活用しましょう。
+            <p class="main_story_message">
+              豊富なテーマ満載のセミナーから<br />住宅購入に関するセミナーをピックアップ。
             </p>
           </div>
-           <div class="main_simulation main_bg">
-            <div class="main_simulation_message">
-              <p></p>
-            </div>
-            <p class="main_subMessage">
-              ビデオ会議しながらその場でプラン出力や、<br>各カテゴリでの絞り込みが可能です。
-            </p>
+        </div>
+        <div class="main_story_02">
+          <div class="main_story_02_img">
+            <img src="@/assets/img/dog_1280.jpg" alt="" />
           </div>
-          <div class="main_story">
-            
-          </div>
+          <p class="main_story_message">
+            「人生の三大資金準備 -住宅資金準備-」<br />資料を見ながらわかりやすく解説してくれます。
+          </p>
+        </div>
       </section>
     </div>
   </div>
