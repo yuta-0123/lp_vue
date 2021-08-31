79f789b6de75b394ebe6b759970f10cf0c3745c1
fukuda <yuta.fukuda.0913@gmail.com>
2021-08-31 21:01:16 +0900

    error

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "~@/assets/scss/_settings.scss";'
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
